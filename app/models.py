747b1c52e0a2c41de8f548cac01f1fad4ed67c99
RafaelVictor15 <rafaelvictor1215@gmail.com>
2025-09-17 10:58:06 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,3 +1,181 @@
 from django.db import models
 
-# Create your models here.
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100)
+    uf = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.nome} - {self.uf}" 
+
+# --------------------------------------------------------------------------------------------------------------------------
+
+class Usuários(models.Model):
+    nome  = models.CharField(max_length=100, verbose_name="Nome do alimento")
+    cpf = models.CharField(max_length=100, verbose_name="É apropriado para") # ------------------------ (como ligar mais de um animal nessa parte dos apropriados)
+    senha = models.CharField(max_length=100, verbose_name="É inapropriado para")
+    email = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Valor energético (Cal)") # --------------------- ()
+    idade = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Açúcares totais (100g)")
+    carboidratoss = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Carboidratos (100g)")
+    proteinas = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Proteínas (100g)")
+    gorduras_totais = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Gorduras totais (100g)") 
+    fibras = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Fibras (100g)")
+    sodio = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Sódio (100g)")
+
+    def __str__(self):
+        return self.alimento
+    
+# --------------------------------------------------------------------------------------------------------------------------   
+    
+class Doenca(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome da doença")
+    sintomas = models.CharField(max_length=100, verbose_name="Sintomas")
+    possiveis_causas = models.CharField(max_length=100, verbose_name="Possíveis causas da doença")
+
+    def __str__(self):
+        return self.nome
+    
+# --------------------------------------------------------------------------------------------------------------------------
+    
+class Animal(models.Model):
+    animal = models.CharField(max_length=100, verbose_name="Animal")
+    raca = models.CharField(max_length=100, verbose_name="Raça")
+    nome_cientifico = models.CharField(max_length=100, verbose_name="Nome científico")
+    porte = models.CharField(max_length=14, verbose_name="Porte (pequeno, médio ou grande)")
+    expectativa_vida = models.CharField(max_length=100, verbose_name="Expectativa média de vida")
+    alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.animal
+    
+# --------------------------------------------------------------------------------------------------------------------------
+    
+class Dieta(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+    quantidade_diaria = models.IntegerField(verbose_name="quantidade diária (g)")
+
+    def __str__(self):
+        return f"{self.alimento} - {self.quantidade_diaria}"
+# --------------------------------------------------------------------------------------------------------------------------
+    
+class Especificacoes(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+
+# --------------------------------------------------------------------------------------------------------------------------   
+    
+class Sugestao(models.Model):
+    titulo = models.CharField(max_length=100, blank=False, null=False)
+    sugestao = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+    
+# --------------------------------------------------------------------------------------------------------------------------
+    
+class Questionario(models.Model):
+    animal = models.CharField(max_length=100, verbose_name="Nome do animal")
+    raça = models.CharField(max_length=100, verbose_name="Raça do animal")
+    porte = models.CharField(max_length=100, verbose_name="Porte do animal")
+    peso = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Peso do animal")
+    idade = models.IntegerField(verbose_name="idade do animal (anos)")
+    possui_doenca = models.CharField(max_length=100, blank=True, null=True, verbose_name="O animal possui alguma doença? Caso não tenha, mantenha o campo em branco")
+    alergia = models.CharField(max_length=100, blank=True, null=True, verbose_name="O animal possui alguma alergia? Caso não tenha, mantenha o campo em branco")
+    dieta = models.ForeignKey(Dieta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.dieta)
+    
+# --------------------------------------------------------------------------------------------------------------------------
+    
+class Artigo(models.Model):
+    nome = models.CharField(max_length=1000, verbose_name="Nome do Artigo")
+    autor = models.CharField(max_length=100, verbose_name="Autor")
+    ano = models.IntegerField(verbose_name="Ano de Publicação")
+    link = models.CharField(max_length=10000, verbose_name="Link do artigo")
+
+    def __str__(self):
+        return self.nome  
+    
+
+
+
+# from django.db import models
+
+# class Cidade(models.Model):
+#     nome = models.CharField(max_length=100)
+#     uf = models.CharField(max_length=2)
+
+#     def __str__(self):
+#         return f"{self.nome} - {self.uf}"
+
+# # --------------------------------------------------------------------------------------------------------------------------
+
+# class Alimento(models.Model):
+#     alimento  = models.CharField(max_length=100, verbose_name="Nome do alimento")
+#     apropriado = models.CharField(max_length=100, verbose_name="É apropriado para") # ------------------------ (como ligar mais de um animal nessa parte dos apropriados)
+#     inapropriado = models.CharField(max_length=100, verbose_name="É inapropriado para")
+#     valor_energetico = models.DecimalField(verbose_name="Valor energético (Cal)") # --------------------- ()
+#     acucares_totais = models.DecimalField(verbose_name="Açúcares totais (100g)")
+#     carboidratoss = models.DecimalField(verbose_name="Carboidratos (100g)")
+#     proteinas = models.DecimalField(verbose_name="Proteínas (100g)")
+#     gorduras_totais = models.DecimalField(verbose_name="Gorduras totais (100g)") 
+#     fibras = models.DecimalField(verbose_name="Proteínas (100g)")
+#     sodio = models.DecimalField(verbose_name="Sódio (100g)")
+
+#     def __str__(self):
+#         return self.alimento
+# # --------------------------------------------------------------------------------------------------------------------------   
+# class Doenca(models.Model):
+#     nome = models.CharField(max_length=100, verbose_name="Nome popular")
+#     nome_cientifico = models.CharField(max_length=100, verbose_name="Nome científico")
+#     sintomas = models.CharField(max_length=100, verbose_name="Sintomas")
+#     possiveis_causas = models.CharField(max_length=100, verbose_name="Possíveis causas da doença")
+
+#     def __str__(self):
+#         return self.nome
+# # --------------------------------------------------------------------------------------------------------------------------
+# class Animal(models.Model):
+#     animal = models.CharField(max_length=100, verbose_name="Animal")
+#     raca = models.CharField(max_length=100, verbose_name="Raça")
+#     nome_cientifico = models.CharField(max_length=100, verbose_name="Nome científico")
+#     porte = models.CharField(max_length=14, verbose_name="Porte (pequeno, médio ou grande)")
+#     expectativa_vida = models.CharField(max_length=100, verbose_name="Expectativa média de vida")
+#     alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.animal
+# # --------------------------------------------------------------------------------------------------------------------------
+# class Dieta(models.Model):
+#     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+#     alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+#     quantidade_diaria = models.IntegerField(verbose_name="quantidade diária (g)")
+
+#     def __str__(self):
+#         return self.nome # ---------------------
+# # --------------------------------------------------------------------------------------------------------------------------
+# class Especificacoes(models.Model):
+#     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+#     alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+# # --------------------------------------------------------------------------------------------------------------------------   
+# class Sugestao(models.Model):
+#     sugestao = models.TextField()
+
+#     def __str__(self):
+#         return self.sugestao
+# # --------------------------------------------------------------------------------------------------------------------------
+# class questionario(models.Model):
+#     animal = models.CharField(max_length=100, verbose_name="Nome do animal")
+#     raça = models.CharField(max_length=100, verbose_name="Nome do animal")
+#     porte = models.CharField(max_length=100, verbose_name="Nome do animal")
+#     peso = models.DecimalField(verbose_name="Gorduras totais (100g)")
+#     idade = models.IntegerField(verbose_name="quantidade diária (g)")
+#     possui_doenca = models.CharField(max_length=100, verbose_name="Nome do animal")
+#     alergia = models.CharField(max_length=100, verbose_name="Nome do animal")
+
+#     def __str__(self):
+#         return self.nome
+# # --------------------------------------------------------------------------------------------------------------------------   
+
+
+
