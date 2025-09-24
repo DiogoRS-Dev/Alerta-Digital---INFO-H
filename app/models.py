e9c54c36dfa83adc1913b06b256a3e15d4b19eac
RafaelVictor15 <rafaelvictor1215@gmail.com>
2025-09-24 11:05:23 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,101 +1,82 @@
 from django.db import models
 
-class Cidade(models.Model):
-    nome = models.CharField(max_length=100)
-    uf = models.CharField(max_length=2)
-
-    def __str__(self):
-        return f"{self.nome} - {self.uf}" 
-
-# --------------------------------------------------------------------------------------------------------------------------
-
-class Usuários(models.Model):
+class Usuario(models.Model):
     nome  = models.CharField(max_length=100, verbose_name="Nome do alimento")
     cpf = models.CharField(max_length=100, verbose_name="É apropriado para") # ------------------------ (como ligar mais de um animal nessa parte dos apropriados)
     senha = models.CharField(max_length=100, verbose_name="É inapropriado para")
-    email = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Valor energético (Cal)") # --------------------- ()
+    email = models.CharField(max_length=100, verbose_name="É inapropriado para") # --------------------- ()
     idade = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Açúcares totais (100g)")
-    carboidratoss = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Carboidratos (100g)")
-    proteinas = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Proteínas (100g)")
-    gorduras_totais = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Gorduras totais (100g)") 
-    fibras = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Fibras (100g)")
-    sodio = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Sódio (100g)")
-
+    
     def __str__(self):
-        return self.alimento
+        return self.nome
     
 # --------------------------------------------------------------------------------------------------------------------------   
     
-class Doenca(models.Model):
+class Administrador(models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome da doença")
-    sintomas = models.CharField(max_length=100, verbose_name="Sintomas")
-    possiveis_causas = models.CharField(max_length=100, verbose_name="Possíveis causas da doença")
-
+    setor = models.CharField(max_length=100, verbose_name="Sintomas")
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.nome
     
 # --------------------------------------------------------------------------------------------------------------------------
     
-class Animal(models.Model):
-    animal = models.CharField(max_length=100, verbose_name="Animal")
-    raca = models.CharField(max_length=100, verbose_name="Raça")
-    nome_cientifico = models.CharField(max_length=100, verbose_name="Nome científico")
-    porte = models.CharField(max_length=14, verbose_name="Porte (pequeno, médio ou grande)")
-    expectativa_vida = models.CharField(max_length=100, verbose_name="Expectativa média de vida")
-    alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
-
+class Acesso(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    dataHora = models.DateTimeField(default=timezone.now)
+    
     def __str__(self):
-        return self.animal
+        return f"{self.usuario.nome} entrou em {self.dataHora}"
     
 # --------------------------------------------------------------------------------------------------------------------------
     
-class Dieta(models.Model):
-    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
-    alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
-    quantidade_diaria = models.IntegerField(verbose_name="quantidade diária (g)")
+class Mensagem(models.Model):
+    mensagem = models.TextField()
+    dataHora = models.DateTimeField(default=timezone.now)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.alimento} - {self.quantidade_diaria}"
 # --------------------------------------------------------------------------------------------------------------------------
     
-class Especificacoes(models.Model):
-    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
-    alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+class Denuncia(models.Model):
+    mensagem = models.CharField(max_length=45)
+    data_hora = models.DateTimeField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Denúncia {self.id} - {self.mensagem}"
 
 # --------------------------------------------------------------------------------------------------------------------------   
     
-class Sugestao(models.Model):
-    titulo = models.CharField(max_length=100, blank=False, null=False)
-    sugestao = models.TextField()
+class Pergunta(models.Model):
+    pergunta = models.CharField(max_length=45)
+    nivel = models.IntegerField()
 
     def __str__(self):
-        return self.titulo
+        return self.pergunta
     
 # --------------------------------------------------------------------------------------------------------------------------
     
-class Questionario(models.Model):
-    animal = models.CharField(max_length=100, verbose_name="Nome do animal")
-    raça = models.CharField(max_length=100, verbose_name="Raça do animal")
-    porte = models.CharField(max_length=100, verbose_name="Porte do animal")
-    peso = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Peso do animal")
-    idade = models.IntegerField(verbose_name="idade do animal (anos)")
-    possui_doenca = models.CharField(max_length=100, blank=True, null=True, verbose_name="O animal possui alguma doença? Caso não tenha, mantenha o campo em branco")
-    alergia = models.CharField(max_length=100, blank=True, null=True, verbose_name="O animal possui alguma alergia? Caso não tenha, mantenha o campo em branco")
-    dieta = models.ForeignKey(Dieta, on_delete=models.CASCADE)
+class Quiz(models.Model):
+    nota = models.FloatField()
+    data_hora = models.DateTimeField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    perguntas = models.ManyToManyField(Pergunta, through="QuizPergunta")
 
     def __str__(self):
-        return str(self.dieta)
+        return f"Quiz {self.id} - Nota: {self.nota}"
     
 # --------------------------------------------------------------------------------------------------------------------------
     
-class Artigo(models.Model):
-    nome = models.CharField(max_length=1000, verbose_name="Nome do Artigo")
-    autor = models.CharField(max_length=100, verbose_name="Autor")
-    ano = models.IntegerField(verbose_name="Ano de Publicação")
-    link = models.CharField(max_length=10000, verbose_name="Link do artigo")
+class QuizPergunta(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nome  
+        return f"Quiz {self.quiz.id} - Pergunta {self.pergunta.id}"
     
 
 
